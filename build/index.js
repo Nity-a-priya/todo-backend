a0a1f816341c42465585b35251c2e39c15ad1cb4
Nity-a-priya <nityapriya.katta@thoughtworks.com>
2024-02-20 23:37:30 +0530

    calendar feature

--- build/index.js
+++ build/index.js
@@ -1,5 +1,4 @@
 let DATA = [];
-let cFlag = [];
 let isEditable = {
   isEdited: false,
 };
@@ -26,7 +25,8 @@ const createElement = (ele) => {
 // improve
 const clickSubmit = async (url, body) => {
   const value = document.getElementById("text").value;
-  if (value != "") {
+  const date = document.getElementById("date").value;
+  if (value != "" || date != "") {
     await postReq(url, body);
 
     hideInput();
@@ -49,6 +49,7 @@ const hideInput = () => {
   const input_div = document.getElementsByClassName("input-div")[0];
   input_div.style.display = "none";
   document.getElementById("text").value = "";
+  document.getElementById("date").value = "";
 
   isEditable.isEdited = false;
   showItems();
@@ -56,13 +57,18 @@ const hideInput = () => {
 
 const handleSubmit = () => {
   const value = document.getElementById("text").value;
+  const date = document.getElementById("date").value;
+  document.querySelector(".add").disabled = false;
+
   let url = "/add";
   let body = {
-    value: value,
+    value,
+    date,
   };
   if (isEditable.isEdited) {
     body = {
       value,
+      date,
       index: isEditable.index,
     };
     url = "/edit";
@@ -72,53 +78,90 @@ const handleSubmit = () => {
 
 const showItems = () => {
   const list = document.getElementsByTagName("ul")[0];
-  let no_of_completed = 0;
+
   list.innerHTML = "";
-  DATA.forEach((item, index) => {
-    const div = createElement("div");
-    div.setAttribute("id", "items");
-    // div.setAttribute("onclick","completed()");
-    div.innerHTML = `<li id = "li" onclick = "completed('${index}')">${item.item}</li><button id="edit" onclick = "handleEdit('${item.item}','${index}')" ><img src="pen-to-square-solid.svg"></button><button id="delete" onclick = "handleDelete('${index}')"><img src="dustbin.jpeg"></button>`;
-    list.appendChild(div);
-    if (item.isCompleted) {
-      showCompleteColour(index);
-      no_of_completed = no_of_completed + 1;
-    }
+  const uniqueDates = new Set();
+  DATA.forEach((item) => {
+    uniqueDates.add(item.date);
+    console.log(item.id+" ---------------"+item.isCompleted);
   });
-  let total = DATA.length;
-  document.getElementsByTagName(
-    "h1"
-  )[0].innerText = `Completed: ${no_of_completed}/${total}`;
+  // displayItemsInEachDate(uniqueDates,list);
+  uniqueDates.forEach((date, index) => {
+    let no_of_completed = 0;
+    const x = DATA.filter((obj) => obj.date === date);
+    console.log(x);
+    const calendar_div = createElement("div");
+    calendar_div.setAttribute("id", "calendar"+index);
+    calendar_div.setAttribute("class","calendar");
+    list.appendChild(calendar_div);
+
+    //  const date2 = date.split("-");
+
+    // const newDate = date2[0] + " " + date2[1] + ", " + date2[2];
+    const calendar_heading = createElement("div");
+    calendar_heading.setAttribute("class","calendar_heading");
+    calendar_div.appendChild(calendar_heading);
+    const newDate = date;
+
+    const calendar_text = createElement("h3");
+    calendar_text.textContent = newDate;
+    calendar_heading.appendChild(calendar_text);
+
+    const completed_count = createElement("h4");
+    completed_count.textContent =  "Completed: " + no_of_completed + "/" + x.length;
+    calendar_heading.appendChild(completed_count);
+
+    x.forEach((item) => {
+      const div = createElement("div");
+      div.setAttribute("class", "row");
+      div.setAttribute("id", "items" + item.id);
+      // div.setAttribute("onclick","completed()");
+      div.innerHTML = `<li id = "li" onclick = "completed('${item.id}', '${index}')">${item.item}</li><button id="edit" onclick = "handleEdit('${item.item}','${item.date}','${item.id}')" ><img src="pen-to-square-solid.svg"></button><button id="delete" onclick = "handleDelete('${item.id}')"><img src="dustbin.jpeg"></button>`;
+      calendar_div.appendChild(div);
+      if (item.isCompleted) {
+        showCompleteColour(item.id, index);
+        no_of_completed = no_of_completed + 1;
+      }
+    });
+    completed_count.textContent =  "Completed: " + no_of_completed + "/" + x.length;
+
+  });
+  
 };
 
-const handleEdit = (item, index) => {
-  document.querySelectorAll("#delete")[index].disabled = true;
+const handleEdit = (text, date, index) => {
+  // document.getElementById("delete"+index).disabled = true;
+  document.getElementById("items"+index).children.delete.disabled=true;
+  document.getElementById("items"+index).style.pointerEvents = "none";
+  document.querySelector(".add").disabled = true;
+
   const input_div = document.getElementsByClassName("input-div")[0];
   input_div.style.display = "";
-  document.getElementById("text").value = item;
+  document.getElementById("text").value = text;
+  document.getElementById("date").value = date;
 
   //-------------------
   isEditable.isEdited = true;
   isEditable.index = index;
   //-------------------
 };
-const completed = async (index) => {
-  showCompleteColour(index);
+const completed = async (id,index) => {
+  showCompleteColour(id,index);
   const url = "/completeSign";
   body = {
-    index,
+    id,
   };
   await postReq(url, body);
   showItems();
 };
-const showCompleteColour = (index) => {
-  const divTag = document.querySelectorAll("#items")[index];
+const showCompleteColour = (id,index) => {
+  const divTag = document.getElementById("items" + id);
   divTag.style.pointerEvents = "none";
   divTag.style.cursor = "none";
   divTag.style.backgroundColor = "#5a6b59";
   divTag.style.textShadow = "none";
 
-  const ul = document.getElementsByClassName("list")[0];
+  const calendarID = document.getElementById("calendar"+index);
   const newDiv = createElement("div");
   newDiv.setAttribute("id", "complete_div");
 
@@ -129,7 +172,7 @@ const showCompleteColour = (index) => {
   newDiv.appendChild(doneTag);
   newDiv.appendChild(divTag);
 
-  ul.appendChild(newDiv);
+  calendarID.appendChild(newDiv);
 };
 
 const handleDelete = async (index) => {
@@ -144,8 +187,11 @@ const handleCancel = () => {
   const input_div = document.getElementsByClassName("input-div")[0];
   input_div.style.display = "none";
   document.getElementById("text").value = "";
+  document.getElementById("date").value = "";
   isEditable.isEdited = false;
-  document.querySelectorAll("#delete").forEach(ele => ele.disabled=false);
+  document.getElementById("items"+isEditable.index).style.pointerEvents = "";
+  document.querySelectorAll("#delete").forEach((ele) => (ele.disabled = false));
+  document.querySelector(".add").disabled = false;
 };
 
 const main = async () => {
--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,24 @@
-let data = [];
+let data = [
+  {
+    id:22,
+    item:"nitya",
+    date:'2024-02-19'
+
+  },{
+    id:23,
+    item:"priya",
+    date:'2024-02-20'
+  },{
+    id:24,
+    item:"venky",
+    date:'2024-02-20'
+  },{
+    id:25,
+    item:"setti",
+    date:'2024-02-19'
+  }
+];
+let id = 0;
 
 const getData = (req, res) => {
   res.send(data);
@@ -6,33 +26,43 @@ const getData = (req, res) => {
 
 const postItem = (req, res) => {
   const item = req.body.value;
+  const date = req.body.date;
   const obj = {
+    id:id+1,
     item,
+    date,
     isCompleted:false
   }
   data.unshift(obj);
+  id++;
   res.send(data);
 };
 
 const replaceItem = (req, res) => {
   const new_item = req.body.value;
-  const index = req.body.index;
+  const new_date = req.body.date;
+  const id = req.body.index;
   const obj = {
+    id,
     item:new_item,
+    date:new_date,
     isCompleted:false
   }
+  const index = data.findIndex( obj => obj.id == id);
   data.splice(index, 1, obj);
   res.send(data);
 };
 
 const deleteData = (req, res) => {
   const id = req.body.index;
-  data.splice(id, 1);
+  const index = data.findIndex( obj => obj.id == id);
+  data.splice(index, 1);
   res.send(data);
 };
 
 const disableItem = (req, res) => {
-  const index = req.body.index;
+  const id = req.body.id;
+  const index = data.findIndex( obj => obj.id == id);
   data[index].isCompleted = true;
   res.send(data);
 };
