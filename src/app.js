3c853e8770e0addc5377a63e63ceeb55a7715199
Nity-a-priya <nityapriya.katta@thoughtworks.com>
2024-03-04 15:53:28 +0530

    Handle multiple users

--- src/app.js
+++ src/app.js
@@ -1,34 +1,21 @@
 const express = require("express");
 const {
+  setCookie,
   getTodos,
   addTodo,
   editTodo,
   deleteTodo,
-  MarkTodoAsDone,
+  MarkTodoAsDone
 } = require("./logic");
 
 const app = express();
 app.use(express.static("build"));
 app.use(express.json());
 
-const cookieCount = 1
 
-app.get("/addCookie", (req, res)=>{
-  res.setHeader('Set-Cookie', 'user4');
-  res.send("Success");
-});
-// middleware
-
-const myMiddleware = (req, res, next) => {
-  const cookieHeader = req.headers.cookie;
-  if (!cookieHeader) {
-    res.setHeader('Set-Cookie', 'user4');
-  }
-  next(); // Pass control to the next middleware function
-};
 
 // Register the middleware
-app.use(myMiddleware);
+app.use(setCookie);
 app.get("/get", getTodos);
 app.post("/delete", deleteTodo);
 app.post("/add", addTodo);
--- src/logic.js
+++ src/logic.js
@@ -1,25 +1,14 @@
-let DATA = [
-  {
-    id: 22,
-    item: "nitya",
-    date: "2024-02-19",
-  },
-  {
-    id: 23,
-    item: "priya",
-    date: "2024-02-20",
-  },
-  {
-    id: 24,
-    item: "venky",
-    date: "2024-02-20",
-  },
-  {
-    id: 25,
-    item: "setti",
-    date: "2024-02-19",
-  },
-];
+// [{cookie:coo,DATA:[{},{}]},{},{}....]
+let USERS = [];
+
+const generateCookie = (() => {
+  let cookieCount = 0;
+  return () => {
+    cookieCount++;
+    return cookieCount;
+  };
+})();
+
 
 const generateId = (() => {
   let id = 0;
@@ -33,13 +22,34 @@ const getMatchingIndex = (todos, id) => {
   return todos.findIndex((todo) => todo.id == id);
 };
 
+const getMatchingUser = (cookieHeader) => {
+  return USERS.findIndex((user) => user.cookie == cookieHeader);
+};
+
+const setCookie = (req, res, next) => {
+  let cookieHeader = req.headers.cookie;
+  if (!cookieHeader) {
+    cookieHeader = `user${generateCookie()}`;
+    USERS.push({ cookie: cookieHeader, DATA:[]});
+    res.setHeader("Set-Cookie", cookieHeader);
+  }
+  req.app.locals.cookie = cookieHeader;
+  next(); // Pass control to the next middleware function
+};
+
+const getuserData = (cookieHeader) => {
+  const user_index = getMatchingUser(cookieHeader);
+  return [...USERS[user_index].DATA];
+};
+
 const getTodos = (req, res) => {
-  const cookieHeader = req.headers.cookie;
-  console.log(cookieHeader)
-  res.send(DATA);
+  const cookieHeader = req.app.locals.cookie;
+  const todosData = getuserData(cookieHeader);
+  res.send(todosData);
 };
 
 const addTodo = (req, res) => {
+  const cookieHeader = req.app.locals.cookie;
   const text = req.body.value;
   const date = req.body.date;
   const id = generateId();
@@ -49,11 +59,16 @@ const addTodo = (req, res) => {
     date,
     isCompleted: false,
   };
-  DATA.unshift(obj);
-  res.send(DATA);
+
+  const data = getuserData(cookieHeader);
+  data.unshift(obj);
+  let user_index = getMatchingUser(cookieHeader);
+  USERS[user_index].DATA = data;
+  res.send(USERS[user_index].DATA);
 };
 
 const editTodo = (req, res) => {
+  const cookieHeader = req.app.locals.cookie;
   const new_text = req.body.value;
   const new_date = req.body.date;
   const id = req.body.index;
@@ -63,26 +78,40 @@ const editTodo = (req, res) => {
     date: new_date,
     isCompleted: false,
   };
-  const index = getMatchingIndex(DATA, id);
-  DATA.splice(index, 1, newTodo);
-  res.send(DATA);
+  let data = getuserData(cookieHeader);
+  const index = getMatchingIndex(data, id);
+  data.splice(index, 1, newTodo);
+  let user_index = getMatchingUser(cookieHeader);
+  USERS[user_index].DATA = data;
+  res.send(USERS[user_index].DATA);
 };
 
 const deleteTodo = (req, res) => {
+  const cookieHeader = req.app.locals.cookie;
   const id = req.body.index;
-  const index = getMatchingIndex(DATA, id);
-  DATA.splice(index, 1);
-  res.send(DATA);
+  let data = getuserData(cookieHeader);
+  const index = getMatchingIndex(data, id);
+  data.splice(index, 1);
+  let user_index = getMatchingUser(cookieHeader);
+  USERS[user_index].DATA = data;
+  res.send(USERS[user_index].DATA);
 };
 
 const MarkTodoAsDone = (req, res) => {
-  const id = req.body.id;
-  const index = getMatchingIndex(DATA, id);
-  DATA[index].isCompleted = true;
-  res.send(DATA);
+  const todoId = req.body.id;
+  const cookieHeader = req.app.locals.cookie;
+  let user_index = getMatchingUser(cookieHeader);
+  const data = getuserData(cookieHeader);
+  const todoIndex = getMatchingIndex(data, todoId);
+  data[todoIndex].isCompleted = true;
+  USERS[user_index].DATA = data;
+  res.send(USERS[user_index].DATA);
 };
 
+
+
 module.exports = {
+  setCookie,
   addTodo,
   editTodo,
   getTodos,
